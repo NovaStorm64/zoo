93a16472d569a74775c6b160e6b4cc3ecaa084b3
Harry Ayres <17U-HAyres@utccambridge.co.uk>
2020-03-17 11:35:46 +0000

    p

---
+++ Environment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    class Environment
+    {
+        
+        List<Animal> animals = new List<Animal>();
+        class environment
+        {
+            public int temperature { get; }
+            public int size { get; }
+            public int visitorNumber { get; set; }
+            public int noOfAnimals { get; set; }
+
+        }
+
+    }
+}
