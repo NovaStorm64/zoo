ff46d1f91fed7f09f4b2590988f860b3af7fbd09
Harry Ayres <17U-HAyres@utccambridge.co.uk>
2020-03-17 11:39:55 +0000

    tanks

---
+++ Tanks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    class Tanks
+    {
+        class tanks : Environment
+        {
+            public int depth { get; }
+
+        }
+    }
+}
